a39657d07702c0b75e1aa7818a30f2a55ea33fc8
Avanish Gupta <avanishgupta606@gmail.com>
2021-10-15 11:42:01 +0530

    initial commit

---
+++ app/src/main/java/com/example/myapplication/adapter/GoiAdapter.java
@@ -0,0 +1,68 @@
+package com.example.myapplication.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.myapplication.R;
+import com.example.myapplication.models.GoiDetail;
+
+import java.util.List;
+
+public class GoiAdapter extends RecyclerView.Adapter<GoiAdapter.GoiViewHolder>
+{
+    LayoutInflater layoutInflator;
+    List<GoiDetail> goiDetails;
+
+
+    public GoiAdapter(Context context,List<GoiDetail> details) {
+      this.layoutInflator= LayoutInflater.from(context);
+      this.goiDetails=details;
+    }
+
+    @NonNull
+    @Override
+    public GoiAdapter.GoiViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+       View view =LayoutInflater.from(parent.getContext()).inflate(R.layout.goiitem,parent,false);
+       return new GoiViewHolder(view);
+
+
+
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull GoiAdapter.GoiViewHolder holder, int position) {
+
+        holder.itemName.setText(goiDetails.get(position).getGroceryName());
+        holder.itemPlace.setText(goiDetails.get(position).getGroceryPlace());
+        holder.itemPrice.setText("₹"+goiDetails.get(position).getGroceryPrice());
+
+
+        holder.itemDate.setText(goiDetails.get(position).getGroceryTime());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return goiDetails.size();
+    }
+
+    public static class GoiViewHolder extends RecyclerView.ViewHolder{
+        TextView itemName,itemPlace,itemPrice,itemDate;
+
+        public GoiViewHolder(@NonNull View itemView) {
+            super(itemView);
+            itemName=itemView.findViewById(R.id.ItemNameView);
+            itemPlace=itemView.findViewById(R.id.ItemPlace);
+            itemPrice=itemView.findViewById(R.id.ItemPrice);
+            itemDate=itemView.findViewById(R.id.ItemDate);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/models/GoiDetail.java
@@ -0,0 +1,66 @@
+package com.example.myapplication.models;
+
+public class GoiDetail {
+
+    private String GroceryName, GroceryPlace, GroceryPrice;
+    private String  GroceryTime;
+
+    public GoiDetail() {
+    }
+
+    public GoiDetail(String groceryName, String groceryPlace, String groceryPrice, String groceryTime) {
+        GroceryName = groceryName;
+        GroceryPlace = groceryPlace;
+        GroceryPrice = groceryPrice;
+        GroceryTime = groceryTime;
+    }
+
+    public String getGroceryName() {
+        return GroceryName;
+    }
+
+    public void setGroceryName(String groceryName) {
+        GroceryName = groceryName;
+    }
+
+    public String getGroceryPlace() {
+        return GroceryPlace;
+    }
+
+    public void setGroceryPlace(String groceryPlace) {
+        GroceryPlace = groceryPlace;
+    }
+
+    public String getGroceryPrice() {
+        return GroceryPrice;
+    }
+
+    public void setGroceryPrice(String groceryPrice) {
+        GroceryPrice = groceryPrice;
+    }
+
+    public String getGroceryTime() {
+        return GroceryTime;
+    }
+
+    public void setGroceryTime(String groceryTime) {
+        GroceryTime = groceryTime;
+    }
+
+//    public String getGroceryName(String commodity) {
+//        return commodity;
+//    }
+//
+//    public String getGroceryPlace(String s) {
+//        return s;
+//    }
+//
+//    public String getGroceryPrice(String modal_price) {
+//        return modal_price;
+//    }
+//
+//    public Long getGroceryTime(Long timestamp) {
+//        return timestamp;
+//    }
+}
+
